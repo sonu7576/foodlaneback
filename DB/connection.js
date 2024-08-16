ec0877671db955aa61841651ab70655e288c7f76
sonu7576 <cksonu00@gmail.com>
2024-08-16 20:15:26 +0530

    foodlaneback

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const connectionString=process.env.Database
+const connectionString=process.env.DATABASE
 
 mongoose.connect(connectionString).then(()=>{
     console.log('restuarant server connected successfully to mongodb atlas');
